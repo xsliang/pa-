ccab7b1c1ac31310490211c6760926ae7352d467
xsliang <xsliang17@hotmail.com>
2018-02-26 10:23:50 +0800

    C032 C047

---
+++ paiza/C/C032.cs
@@ -0,0 +1,103 @@
+﻿
+//paiza カードは毎日のお買い物でポイントがたまるお得なポイントカードです。
+//ポイントの付与基準は商品の種類によって以下のように異なります。
+
+
+
+//ポイントは以下の手順で計算されます。
+
+//1. 商品の種類ごとの合計購入金額を計算する。
+//2. それぞれのポイント付与基準に基づき商品の種類ごとの付与ポイントを計算する。
+//3. 商品の種類ごとの付与ポイントを合計する。(100円未満は切り捨て)
+
+//買い物内訳と合計ポイントの例を見てみましょう。
+
+//商品 1: 食料品 200 円
+//商品 2: 書籍 240 円
+//商品 3: 食料品 120 円
+//商品 4: その他 460 円
+//商品 5: 書籍 240 円
+//商品 6: 衣類 3200 円
+
+//食料品合計 320 円 → 15 ポイント
+//書籍合計 480 円 → 12 ポイント
+//衣類合計 3200 円 → 64 ポイント
+//その他合計 460 円 → 4 ポイント => 合計ポイント → 95 ポイント
+//このように購入した商品の種類と金額のリストが与えられるので、その買い物で付与されるポイントの合計を求めてください。 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paiza.C
+{
+    class C032
+    {
+        public static void Main()
+        {
+            var line = System.Console.ReadLine();
+            int N = Convert.ToInt32(line);
+            if (N < 0 || N > 1000)
+            {
+                return;
+            }
+
+            int n1 = 0;
+            int n2 = 0;
+            int n3 = 0;
+            int n4 = 0;
+
+
+            for (int i = 0; i < N; i++)
+            {
+                line = System.Console.ReadLine();
+                int v = Convert.ToInt32(line.Split(' ')[0]);
+                int p = Convert.ToInt32(line.Split(' ')[1]);
+
+                if (v < 0 || v > 3)
+                {
+                    return;
+                }
+
+                if (p < 10 || p > 10000 || p % 10 != 0)
+                {
+                    return;
+                }
+
+                //if (p < 100)
+                //{
+                //    continue;
+                //}
+
+                // decimal temp = Math.Floor(p / 100);
+
+                switch (v)
+                {
+                    case 0:
+                        n1 = n1 + p;
+                        break;
+                    case 1:
+                        n2 = n2 + p;
+                        break;
+                    case 2:
+                        n3 = n3 + p;
+                        break;
+                    case 3:
+                        n4 = n4 + p;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            n1 = Convert.ToInt32(Math.Floor(n1 / 100.0) * 5);
+            n2 = Convert.ToInt32(Math.Floor(n2 / 100.0) * 3);
+            n3 = Convert.ToInt32(Math.Floor(n3 / 100.0) * 2);
+            n4 = Convert.ToInt32(Math.Floor(n4 / 100.0) * 1);
+
+            Console.WriteLine(n1 + n2 + n3 + n4);
+        }
+    }
+}
---
+++ paiza/C/C047.cs
@@ -0,0 +1,45 @@
+﻿//あなたは霧島京子たちが通うパイザ女学院の用務員として、校舎に備え付けのエレベーターを管理しています。
+//エレベーターが一定の距離を動くと、定期点検をしなければなりません。
+//エレベーターの動きのログをもとに、エレベーターが何階分の距離を動いたか計算するプログラムを完成させてください。
+//ただし、エレベーターは最初は必ず 1 階にいるものとします。
+
+//以下は入力例 1 を図で表したものです。
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paiza.C
+{
+    class C047
+    {
+        public static void Main()
+        {
+            var line = System.Console.ReadLine();
+            int N = Convert.ToInt32(line);
+            if (N < 0 || N > 100)
+            {
+                return;
+            }
+
+            int count = 0;
+            int first = 1;
+            for (int i = 0; i < N; i++)
+            {
+                line = System.Console.ReadLine();
+                int f = Convert.ToInt32(line);
+                if (f < 0 || f > 100)
+                {
+                    return;
+                }
+
+                count = count + (Math.Abs(f - first));
+                first = f;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
