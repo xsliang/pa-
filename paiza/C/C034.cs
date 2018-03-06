d791e2b16aec261c1b3ac7322fe409d50f45b957
xsliang <xsliang17@hotmail.com>
2018-03-06 11:58:48 +0800

    C034 C040

---
+++ paiza/C/C034.cs
@@ -0,0 +1,92 @@
+﻿//あなたは小学校一年生の先生です。今週の授業で、足し算と引き算を教えます。あなたは、足し算と引き算を用いた宿題を作る必要があり、そのためのプログラムを書くことにしました。
+
+//以下の手順で問題をランダム生成するプログラムはもうできているのですが、その答えを求めるプログラムはまだできていません。答えを求めるプログラムを書いてください。
+
+//[問題生成の手順]
+
+//1. 正しい式 a + b = c, あるいは a - b = c(a, b, c, は整数) を生成する
+//2. a, b, c のうちいずれか 1 つを空欄にする
+
+//この空欄に入れるべき整数が「答え」となります。
+
+//ここに、足し算、引き算の問題について例を一つずつ示します。
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paiza.C
+{
+    class C034
+    {
+        static void Main(string[] args)
+        {
+            var line = System.Console.ReadLine();
+
+            string[] lines = line.Split(' ');
+
+            string temp = "1234567890x";
+
+            string a = lines[0];
+            if (temp.Contains(a)==false)
+            {
+                return;
+            }
+
+            string op= lines[1];
+            if ("+-".Contains(op)==false)
+            {
+                return;
+            }
+
+            string b = lines[2];
+            if (temp.Contains(b) == false)
+            {
+                return;
+            }
+
+            string c = lines[4];
+            if (temp.Contains(c) == false)
+            {
+                return;
+            }
+
+            int result = 0;
+
+            if (op=="+")
+            {
+                if (a=="x")
+                {
+                    result = Convert.ToInt32(c) - Convert.ToInt32(b);
+                }
+                else if (b=="x")
+                {
+                    result = Convert.ToInt32(c) - Convert.ToInt32(a);
+                }
+                else
+                {
+                    result = Convert.ToInt32(a) + Convert.ToInt32(b);
+                }
+            }
+            else
+            {
+                if (a == "x")
+                {
+                    result = Convert.ToInt32(c) + Convert.ToInt32(b);
+                }
+                else if (b == "x")
+                {
+                    result = Convert.ToInt32(a) - Convert.ToInt32(c);
+                }
+                else
+                {
+                    result = Convert.ToInt32(a) - Convert.ToInt32(b);
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ paiza/C/C040.cs
@@ -0,0 +1,75 @@
+﻿//何やら子供たちが集まって背比べをしています。話を聞いてみると、一人の子供をのぞいて他の子供の身長は正確にわかっており、その一人の子供の身長を他の子どもと比べることで測ろうとしているようです。手助けをしてあげましょう。
+
+//例)
+
+//図1
+
+//ここでは 6 人の子供の身長がわかっており、この子供たちともう一人の子供の身長を比較すると上のようになりました。これらの結果をまとめると身長のわかっていない子供の身長は 116.9cm 以上 117.6cm 以下であることがわかります(入力例 1)。
+
+//他の同じような状況の子供たちのためにもこれを自動化するプログラムをつくりましょう。
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paiza.C
+{
+    class C040
+    {
+        static void Main(string[] args)
+        {
+            var line = System.Console.ReadLine();
+            int N = Convert.ToInt32(line);
+            if (N < 2 || N > 100)
+            {
+                return;
+            }
+
+            decimal ge = 0;
+            decimal le = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                line = System.Console.ReadLine();
+                string c = line.Split(' ')[0];
+                decimal temp = Convert.ToDecimal(line.Split(' ')[1]);
+
+                if (temp < 100 || temp > 200)
+                {
+                    return;
+                }
+
+                if (c == "ge")
+                {
+                    if (ge == 0)
+                    {
+                        ge = temp;
+                    }
+                    else if (ge < temp)
+                    {
+                        ge = temp;
+                    }
+                }
+                else if (c == "le")
+                {
+                    if (le == 0)
+                    {
+                        le = temp;
+                    }
+                    else if (le > temp)
+                    {
+                        le = temp;
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            Console.WriteLine(ge + " " + le);
+        }
+    }
+}
