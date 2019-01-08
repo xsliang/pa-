5d85da28ea2b45476f67affdb9d702ec38a12065
xsliang <xsliang17@hotmail.com>
2019-01-08 14:26:20 +0800

    C043 C056

---
+++ paiza/C/C043.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paiza.C
+{
+    class C043
+    {
+        public static void C043Main()
+        {
+            try
+            {
+                var line = System.Console.ReadLine();
+                int N = Convert.ToInt32(line);
+                if (N >= 1 && N <= 1000)
+                {
+                    line = System.Console.ReadLine();
+                    Dictionary<string, int> result = new Dictionary<string, int>();
+
+                    if (line.Split(' ').Length == N)
+                    {
+                        string[] input = line.Split(' ');
+                        foreach (var item in input)
+                        {
+                            int number = Convert.ToInt32(item);
+                            if (number >= 1 && number <= 10000)
+                            {
+                                if (result.ContainsKey(number.ToString()))
+                                {
+                                    result[number.ToString()] = result[number.ToString()] + 1;
+                                }
+                                else
+                                {
+                                    result[number.ToString()] = 1;
+                                }
+                            }
+                        }
+
+                        var temp = result.OrderByDescending(x => x.Value).ToList();
+
+                        List<int> result2 = new List<int>();
+                        int max = temp[0].Value;
+                        //result2.Add(temp[0].Key);
+                        foreach (var item in temp)
+                        {
+                            if (item.Value == max)
+                            {
+                                result2.Add(Convert.ToInt32(item.Key));
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        result2.Sort();
+
+                        System.Console.WriteLine(String.Join(" ",result2.ToArray()));
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
---
+++ paiza/C/C056.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paiza.C
+{
+    class C056
+    {
+        public static void C056Main()
+        {
+            try
+            {
+                var line = System.Console.ReadLine();
+                if (line.Split(' ').Length == 2)
+                {
+                    int N = Convert.ToInt32(line.Split(' ')[0]);
+                    int M = Convert.ToInt32(line.Split(' ')[1]);
+                    List<int> number = new List<int>();
+                    if (N >= 1 && N <= 100 && M >= 0 && M <= 100)
+                    {
+                        for (int i = 0; i < N; i++)
+                        {
+                            line = System.Console.ReadLine();
+                            if (line.Split(' ').Length == 2)
+                            {
+                                int a = Convert.ToInt32(line.Split(' ')[0]);
+                                int b = Convert.ToInt32(line.Split(' ')[1]);
+
+                                if (a >= 0 && a <= 100 && b >= 0 && b <= 15)
+                                {
+                                    if (a - (5 * b) >= M)
+                                    {
+                                        number.Add(i + 1);
+                                    }
+                                    else if (M == 0)
+                                    {
+                                        number.Add(i + 1);
+                                    }
+                                }
+                            }
+
+                        }
+
+                        foreach (var item in number)
+                        {
+                            System.Console.WriteLine(item);
+                        }
+                    }
+                }
+            }
+            catch
+            {
+            }
+
+
+        }
+    }
+}
