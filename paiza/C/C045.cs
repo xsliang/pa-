bf5f094c176e7ef95a314c8f62d36a8d77943a2f
xsliang <xsliang17@hotmail.com>
2019-01-11 17:32:16 +0800

    C045 C052

---
+++ paiza/C/C045.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paiza.C
+{
+    class C045
+    {
+        public static void C045Main()
+        {
+            try
+            {
+                var line = System.Console.ReadLine();
+                int n = Convert.ToInt32(line.Split(' ')[0]);
+                int s = Convert.ToInt32(line.Split(' ')[1]);
+                int p = Convert.ToInt32(line.Split(' ')[2]);
+
+                if (n >= 0 && n <= 10000 && s >= 1 && s <= 10000 && p >= 1 && p <= 10000)
+                {
+                    int temp = s * (p - 1);
+
+                    if (temp > n)
+                    {
+                        Console.WriteLine("none");
+                    }
+                    else
+                    {
+                        List<int> templist = new List<int>();
+                        for (int i = 0; i < s; i++)
+                        {
+                            if (temp + i + 1 > n)
+                            {
+                                break;
+                            }
+                            templist.Add(temp + i + 1);
+                        }
+
+                        Console.WriteLine(string.Join(" ", templist.ToArray()));
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
---
+++ paiza/C/C052.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paiza.C
+{
+    class C052
+    {
+        public static void C052Main()
+        {
+            try
+            {
+                var line = System.Console.ReadLine();
+                int H = Convert.ToInt32(line.Split(' ')[0]);
+                int W = Convert.ToInt32(line.Split(' ')[1]);
+                var line2 = System.Console.ReadLine();
+                int dy = Convert.ToInt32(line2.Split(' ')[0]);
+                int dx = Convert.ToInt32(line2.Split(' ')[1]);
+
+                if (H >= 1 && H <= 10000 && W >= 1 && W <= 10000 && dy >= -H && dy <= H && dx >= -W && dx <= W)
+                {
+                    int result = (H * Math.Abs(dx)) + (W * Math.Abs(dy)) - (Math.Abs(dx * dy));
+
+                    Console.WriteLine(result);
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
