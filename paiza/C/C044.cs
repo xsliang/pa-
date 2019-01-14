82d30995da8f9f1751f1c7ccea7fa72fca4a939e
xsliang <xsliang17@hotmail.com>
2019-01-14 10:28:39 +0800

    C044

---
+++ paiza/C/C044.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paiza.C
+{
+    class C044
+    {
+        public static void Main()
+        {
+            try
+            {
+                var line = System.Console.ReadLine();
+                int N = Convert.ToInt32(line);
+                if (N >= 2 && N <= 10)
+                {
+                    int countPaper = 0;
+                    int countRock = 0;
+                    int countScissorsr = 0;
+
+
+                    for (int i = 0; i < N; i++)
+                    {
+                        line = System.Console.ReadLine();
+
+                        if (line == "paper")
+                        {
+                            countPaper++;
+                        }
+                        else if (line == "rock")
+                        {
+                            countRock++;
+                        }
+                        else if (line == "scissors")
+                        {
+                            countScissorsr++;
+                        }
+                    }
+
+                    if (countPaper > 0 && countRock > 0 && countScissorsr > 0)
+                    {
+                        System.Console.WriteLine("draw");
+                    }
+                    else if (countPaper > 0 && countRock > 0)
+                    {
+                        System.Console.WriteLine("paper");
+                    }
+                    else if (countPaper > 0 && countScissorsr > 0)
+                    {
+                        System.Console.WriteLine("scissors");
+                    }
+                    else if (countRock > 0 && countScissorsr > 0)
+                    {
+                        System.Console.WriteLine("rock");
+                    }
+                }
+            }
+            catch
+            {
+            }
+
+        }
+    }
+}
