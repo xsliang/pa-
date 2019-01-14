d14e675901d26eb370b6370d6905852e2e1ba0df
xsliang <xsliang17@hotmail.com>
2019-01-14 16:46:35 +0800

    C051 C053

---
+++ paiza/C/C051.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paiza.C
+{
+    class C051
+    {
+        public static void C051Main()
+        {
+            try
+            {
+                var line = System.Console.ReadLine();
+                int a = Convert.ToInt32(line.Split(' ')[0]);
+                int b = Convert.ToInt32(line.Split(' ')[1]);
+                int c = Convert.ToInt32(line.Split(' ')[2]);
+                int d = Convert.ToInt32(line.Split(' ')[3]);
+
+                if (a>=1 && a<=9 && b >= 1 && b <= 9 && c >= 1 && c <= 9 && d >= 1 && d <= 9)
+                {
+                    List<int> result = new List<int>();
+
+                    result.Add((a * 10 + b) + (c * 10 + d));
+                    result.Add((a * 10 + b) + (d * 10 + c));
+                    result.Add((a * 10 + c) + (b * 10 + d));
+                    result.Add((a * 10 + c) + (d * 10 + b));
+                    result.Add((a * 10 + d) + (b * 10 + c));
+                    result.Add((a * 10 + d) + (c * 10 + b));
+                    result.Add((c * 10 + a) + (b * 10 + d));
+
+                    result.Add((c * 10 + d) + (b * 10 + a));
+
+                    result.Add((d * 10 + b) + (c * 10 + a));
+                    result.Add((d * 10 + a) + (b * 10 + c));
+                    result.Add((d * 10 + a) + (c * 10 + b));
+                    result.Add((d * 10 + c) + (b * 10 + a));
+
+                    result.Sort();
+
+                    System.Console.WriteLine(result[11]);
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
---
+++ paiza/C/C053.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paiza.C
+{
+    class C053
+    {
+        public static void C053Main()
+        {
+            try
+            {
+                var line0 = System.Console.ReadLine();
+                int N = Convert.ToInt32(line0);
+                var line = System.Console.ReadLine();
+                var input = line.Split(' ');
+
+                if (input.Length!=N)
+                {
+                    return;
+                }
+
+                if (N >= 1 && N <= 100)
+                {
+                    bool haveX10 = false;
+
+                    bool haveZero = false;
+
+                    int max = 0;
+                    int all = 0;
+                    foreach (var item in input)
+                    {
+                        if (item == "x10" && haveX10 == true)
+                        {
+                            return;
+                        }
+                        if (item == "0" && haveZero == true)
+                        {
+                            return;
+                        }
+                        if (item== "x10" && haveX10==false)
+                        {
+                            haveX10 = true;
+                            continue;
+                        }
+                        if (item == "0" && haveZero == false)
+                        {
+                            haveZero = true;
+                            continue;
+                        }
+
+                        int input_temp = Convert.ToInt32(item);
+
+                        if (input_temp<0 || input_temp>100)
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            if (input_temp>max)
+                            {
+                                max = input_temp;
+                            }
+                            all = all + input_temp;
+                        }
+                    }
+
+                    if (haveZero)
+                    {
+                        all = all - max;
+                    }
+                    if (haveX10)
+                    {
+                        all = all * 10;
+                    }
+
+                    System.Console.WriteLine(all);
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
