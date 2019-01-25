ef3832fc041519f1e2b304282ae25f11f64606fc
xsliang <xsliang17@hotmail.com>
2019-01-25 10:27:02 +0800

    C059 D115

---
+++ paiza/C/C059.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paiza.C
+{
+    class C059
+    {
+        public static void C059Main()
+        {
+            var line = System.Console.ReadLine();
+            int N = Convert.ToInt32(line);
+            int count1 = 0;
+            int count2 = 0;
+            int count3 = 0;
+            int count4 = 0;
+            if (N >= 2 && N <= 50)
+            {
+                List<string> inputs = new List<string>();
+
+
+
+                for (int i = 0; i < N; i++)
+                {
+                    line = System.Console.ReadLine();
+
+                    if (line.Length != 4)
+                    {
+                        return;
+                    }
+
+                    for (int j = 0; j < 4; j++)
+                    {
+                        var item= line[j].ToString();
+
+                        if (item != "0" && item != "1")
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            if (item == "1")
+                            {
+                                switch (j)
+                                {
+                                    case 0:
+                                        count1++;
+                                        break;
+                                    case 1:
+                                        count2++;
+                                        break;
+                                    case 2:
+                                        count3++;
+                                        break;
+                                    case 3:
+                                        count4++;
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+                        }
+                    }
+
+    
+                }
+
+                Console.WriteLine((count1 % 2).ToString() + (count2 % 2).ToString() + (count3 % 2).ToString() + (count4 % 2).ToString());
+
+
+
+            }
+        }
+    }
+}
---
+++ paiza/D/D115.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paiza.D
+{
+    class D115
+    {
+        static void D115Main()
+        {
+            try
+            {
+                var line = System.Console.ReadLine();
+                double N = Convert.ToInt32(line);
+                if (N >= 2 && N <= 50)
+                {
+                    Console.WriteLine(N / 2);
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
