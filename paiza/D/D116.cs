2ebdc5f229ba94ba2a008870e54a4d053d4c8f06
xsliang <xsliang17@hotmail.com>
2019-02-14 10:09:15 +0800

    D116

---
+++ paiza/D/D116.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paiza.D
+{
+    class D116
+    {
+        static void D116Main()
+        {
+            try
+            {
+                var line = System.Console.ReadLine();
+                double N = Convert.ToInt32(line);
+                if (N >= 100 && N <= 100000)
+                {
+                    Console.WriteLine(N.ToString().Substring(0,N.ToString().Length-2)+"00");
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
