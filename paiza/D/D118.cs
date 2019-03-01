620187a099e25f2db285c0c8a3ab061ee731ff57
xsliang <xsliang17@hotmail.com>
2019-03-01 11:44:27 +0800

    D118

---
+++ paiza/D/D118.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paiza.D
+{
+    class D118
+    {
+        static void D118Main()
+        {
+            try
+            {
+                var line = System.Console.ReadLine();
+                var line2 = System.Console.ReadLine();
+                int n = Convert.ToInt32(line2);
+                if (line == "S")
+                {
+                    if (n >= 1 && n <= 64)
+                    {
+                        Console.WriteLine((1926 + (n - 1)));
+                    }
+                }
+                else if (line == "H")
+                {
+                    if (n >= 1 && n <= 31)
+                    {
+                        Console.WriteLine((1989 + (n - 1)));
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
