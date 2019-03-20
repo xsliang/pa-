ff9f6f78c2636c75b32c59019d9966817157d8b3
xsliang <xsliang17@hotmail.com>
2019-03-20 15:09:01 +0900

    D119

---
+++ paiza/D/D119.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paiza.D
+{
+    class D119
+    {
+        static void D119Main()
+        {
+            try
+            {
+                string pi = "3.141592653589793";
+                var line = System.Console.ReadLine();
+                int n = Convert.ToInt32(line);
+                if (n >= 1 && n <= 15)
+                {
+                    Console.WriteLine(pi.Substring(0, n + 2));
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
