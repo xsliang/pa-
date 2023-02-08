f0986125770968ed6b27b23a2a84f21c72dd92a2
xiao songliang <xsliang17@hotmail.com>
2023-02-08 22:48:24 +0800

    Dtest

---
+++ paiza/D/D174.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paiza.D
+{
+    public class D174
+    {
+        public static void Main()
+        {
+            var line = Console.ReadLine();
+            int k = int.Parse(line);
+            if (k>=1 & k<=100)
+            {
+                Console.WriteLine(k*1500);
+            }
+        }
+    }
+}
---
+++ paiza/D/D210.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paiza.D
+{
+    public class D210
+    {
+        public static void D210Main()
+        {
+            var line = Console.ReadLine();
+            int N = int.Parse(line);
+            if (100 >=100 && N<=10000)
+            {
+                Console.WriteLine(12*N);
+            }
+        }
+    }
+}
